5980dea42d07c8ed89ad583712790e611b143e3d
Nik Sharma <sharma7n@gmail.com>
2018-09-02 22:32:47 +0000

    use sqlite

--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': os.environ['POSTGRES_HOST'],
-        'PORT': os.environ['POSTGRES_PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.environ['DJANGO_SQLITE_DB'],
     },
 }
 

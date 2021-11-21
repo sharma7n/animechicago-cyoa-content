dbba7e9e1ddd3ccf685ef84b88388ae166f6c159
Nik <sharma7n@gmail.com>
2021-11-21 22:55:04 +0000

    gitpod fix

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if os.environ.get('DJANGO_GITPOD', False):
+    STATIC_URL = '.static/'
+
 if not os.environ.get('DJANGO_GITPOD', False):
-    STATIC_URL = '/.static/'
     django_heroku.settings(locals())

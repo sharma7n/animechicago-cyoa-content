a0eeb6adc80d2a4b0a23e24a3e826dfb41a4c585
Nik <sharma7n@gmail.com>
2021-12-02 05:36:17 +0000

    hotfix

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,9 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Gitpod
 

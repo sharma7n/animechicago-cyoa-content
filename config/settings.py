43b7a4b80859af8cf699e20cfd27a7924b5ef906
Nik <sharma7n@gmail.com>
2021-12-03 02:23:07 +0000

    hotfix static

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
